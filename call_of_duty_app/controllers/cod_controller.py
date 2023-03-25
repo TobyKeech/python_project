8bbb03a490ee046010c08f847c931c292c929d32
Toby Keech <toby.keech18@gmail.com>
2023-03-25 09:58:30 +0000

    added id route and show html

--- call_of_duty_app/controllers/cod_controller.py
+++ call_of_duty_app/controllers/cod_controller.py
@@ -6,10 +6,15 @@ from models.cod_profile import CodProfile
 
 from flask import Blueprint
 
-codprofiles_blueprint = Blueprint("codprofiles", __name__)
+codprofiles_blueprint = Blueprint("", __name__)
 
 @codprofiles_blueprint.route("/codprofiles")
 def codprofiles():
     codprofiles = cod_profile_repository.select_all() 
     return render_template("codprofiles/index.html", all_codprofiles = codprofiles)
+
+@codprofiles_blueprint.route("/codprofiles/<id>")
+def show_profile(id):
+    codprofile = cod_profile_repository.select(id)
+    return render_template("codprofiles/show.html", solo_profile= codprofile)
 
