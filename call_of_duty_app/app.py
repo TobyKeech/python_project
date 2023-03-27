4d3bb0398836cff8217865136011f9caa7744eb5
Toby Keech <toby.keech18@gmail.com>
2023-03-27 10:41:02 +0100

    added weapon inventory link

--- call_of_duty_app/app.py
+++ call_of_duty_app/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
 from controllers.cod_controller import codprofiles_blueprint
+from controllers.weapon_controller import weapons_blueprint
 
 app = Flask(__name__)
 
 app.register_blueprint(codprofiles_blueprint)
+app.register_blueprint(weapons_blueprint)
 
 @app.route('/')
 def home():
---
+++ call_of_duty_app/controllers/weapon_controller.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, redirect, request
+from repositories import cod_profile_repository
+from repositories import platform_repository
+from repositories import weapon_repository
+from models.weapon import Weapon
+
+from flask import Blueprint
+
+weapons_blueprint = Blueprint("weapons", __name__)
+
+@weapons_blueprint.route("/weapons")
+def weapons():
+    weapons = weapon_repository.select_all() 
+    return render_template("weapons/index.html", all_weapons = weapons)
